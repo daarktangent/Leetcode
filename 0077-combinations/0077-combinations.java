d6e96fe5b800ba793f0aabfe4106c91cfe15bc17
aman kumar jha <97963877+daarktangent@users.noreply.github.com>
2023-08-02 03:02:46 +0530

    Time: 16 ms (78.75%), Space: 45.4 MB (51.05%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,29 +1,19 @@
 class Solution {
-    public void travel(int n,int k, List<List<Integer>> ans,List<Integer> temp,int curr,Set<List<Integer>> map )
-    {
-        
-        if(temp.size()==k)
-        {
-            List<Integer> combination = new ArrayList<>(temp);
-            map.add(combination);
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(n, k, 1, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int n, int k, int start, List<Integer> combination, List<List<Integer>> result) {
+        if (combination.size() == k) {
+            result.add(new ArrayList<>(combination));
             return;
         }
-        for(int i=curr;i<=n;i++)
-        {
-            temp.add(i);
-            travel(n,k,ans,temp,i+1,map);
-            temp.remove(temp.size()-1);
+        for (int i = start; i <= n; i++) {
+            combination.add(i);
+            backtrack(n, k, i + 1, combination, result);
+            combination.remove(combination.size() - 1);
         }
-    }
-    public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        Set<List<Integer>> map = new HashSet<>();
-        for(int i=1;i<=n;i++)
-        {
-            travel(n,k,ans,temp,i,map);
-        }
-        ans.addAll(map);
-        return ans;
     }
 }
