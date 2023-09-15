450a7d09506389c780ed794c8ad0e1314903d7d7
aman kumar jha <97963877+daarktangent@users.noreply.github.com>
2023-09-15 13:12:21 +0530

    Time: 49 ms (5.54%), Space: 44.9 MB (5.36%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,84 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int [][] visited  = new int[grid.length][grid[0].length];
+        Queue<List<Integer>> queue = new LinkedList<>();
+        // for(int [] x:visited)
+        // {
+        //     for(int y:x)
+        //     {
+        //         System.out.print(y+" ");
+        //     }
+        //     System.out.println(" ");
+        // }
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    queue.offer(Arrays.asList(i,j));
+                }
+            }
+        }
+        int time=0;
+        while(!queue.isEmpty())
+        {
+            int size = queue.size();
+            while(size>0)
+            {
+                List<Integer> temp = new ArrayList<>();
+                System.out.println(queue);
+                temp = queue.poll();
+                int x = temp.get(0);
+                int y = temp.get(1);
+                if(visited[x][y]==0)
+                {
+                    System.out.println(x+"  "+ y);
+                    visited[x][y]=1;
+                    if(x+1<grid.length&&(grid[x+1][y]==1)&&visited[x+1][y]==0)
+                    {
+                         System.out.println("in first "+(x+1)+" "+ y);
+                        grid[x+1][y]=2;
+                        queue.offer(Arrays.asList(x+1,y));
+                    }
+                    if(x-1>=0&&(grid[x-1][y]==1)&&visited[x-1][y]==0)
+                    {
+                        System.out.println("in second "+(x-1)+" "+ y);
+                         grid[x-1][y]=2;
+                        queue.offer(Arrays.asList(x-1,y));
+                    }
+                    if(y+1<grid[0].length&&(grid[x][y+1]==1)&&visited[x][y+1]==0)
+                    {
+                       System.out.println("in third "+(x)+" "+ (y+1));
+                         grid[x][y+1]=2;
+                         queue.offer(Arrays.asList(x,y+1));
+                    }
+                     if(y-1>=0&&(grid[x][y-1]==1)&&visited[x][y-1]==0)
+                    {
+                         System.out.println("in fourth "+(x)+" "+ (y-1));
+                          grid[x][y-1]=2;
+                         queue.offer(Arrays.asList(x,y-1));
+                    }
+                }
+                size--;
+            }
+           
+            time++;
+        }
+        for(int [] x:grid)
+        {
+            for(int y:x)
+            {
+                if(y==1)
+                {
+                    return -1;
+                }
+            }
+        }
+        if(time==0)
+        {
+            return 0;
+        }
+        return time-1;
+    }
+}
