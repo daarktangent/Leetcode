b57575bf67e13ec8b659c075bf5d1d2b131a2108
aman kumar jha <97963877+daarktangent@users.noreply.github.com>
2023-09-22 09:15:11 +0530

    Time: 1 ms (93.49%), Space: 43.8 MB (58.85%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,30 +1,33 @@
 class Solution {
-    public static void swapIntegers(int[] arr, int index1, int index2) {
-        int temp = arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = temp;
+    public void swap(int[] nums,int index1,int index2)
+    {
+        int temp = nums[index2];
+        nums[index2]=nums[index1];
+        nums[index1] = temp;
     }
-    public void travel(int[] nums,List<List<Integer>> ans,int n)
+    public void travel(int [] nums,List<List<Integer>> ans,int n) 
     {
         if(n==nums.length)
         {
-           List<Integer> combination = new ArrayList<>();
-            for (int num : nums) {
-                combination.add(num);
+            List<Integer> combination = new ArrayList<>();
+            for(int x:nums)
+            {
+                combination.add(x);
             }
             ans.add(combination);
-            return;
+            return ;
         }
         for(int i=n;i<nums.length;i++)
         {
-            swapIntegers(nums,i,n);
+            swap(nums,i,n);
             travel(nums,ans,n+1);
-            swapIntegers(nums,i,n);
+            swap(nums,i,n);
         }
-        
     }
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums);
+        List<List<Integer> > ans = new ArrayList<>();
+        System.out.println(Arrays.toString(nums));
         travel(nums,ans,0);
         return ans;
     }
